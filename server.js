e4f06440ddc8fde34ca15bda05b7c0482d9544d0
MatveyUtkov <103751684+MatveyUtkov@users.noreply.github.com>
2022-05-10 20:13:24 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require("axios");
 const app = express();
-const port=3000
+var port = process.env.PORT || 3000;
 const UserController = require('./controllers/User')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -138,7 +138,5 @@ app.post('/music', (req, res) => {
         res.send("First game is " + music1)
     });
 });
-app.listen(port, function () {
-    console.log(`app launched at address: http://localhost:${port}`)
-})
+app.listen(port);
 
